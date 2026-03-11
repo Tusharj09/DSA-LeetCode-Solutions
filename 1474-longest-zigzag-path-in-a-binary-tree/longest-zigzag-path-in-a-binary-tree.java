fea2058a1b7bb8e4612f5d072efa7cfbbe6e9416
Tusharj09 <36297059+Tusharj09@users.noreply.github.com>
2026-03-11 23:00:51 +0530

    Time: 4 ms (97.68%) | Memory: 58.2 MB (31.09%) - LeetSync

--- 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.java
+++ 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.java
@@ -1,51 +1,24 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-
-
-
 class Solution {
-    int maxi= 0;
+    int pathLength = 0;
 
-    public void dfs(TreeNode node ,  boolean goleft, int steps ) {
-
-        if(node == null) return ;
-
-        maxi = Math.max(maxi,steps);
-
-        if(goleft) {
-            dfs(node.left,false,steps+1);
-            dfs(node.right,true, 1);
-        }
-        else {
-             dfs(node.left,false,1);
-            dfs(node.right,true, steps+1);
+    private void dfs(TreeNode root, int left, int right) {
+        if (root == null) {
+            return;
         }
 
+        pathLength = Math.max(pathLength, Math.max(left, right));
+
+        if (root.left != null) {
+            dfs(root.left, right + 1, 0);
+        } 
+        
+        if (root.right != null) {
+            dfs(root.right, 0, left + 1);
+        }
     }
-
-
 
     public int longestZigZag(TreeNode root) {
-
-  
-
-
-        dfs(root,true,0);
-
-
-        return maxi;
+        dfs(root, 0, 0);
+        return pathLength;
     }
-}
+};
